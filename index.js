d52c50de67f2d7c45b8ef7184e94b36fe7d6c294
Pekka <nospam@astudios.org>
2017-09-16 16:23:34 +0300

    Better notification

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const opn = require('opn')
 const subs = process.argv[2].split(',').map(sub => sub.trim())
 const responses = {}
 
-const notifyForSub = (sub) => {
+const notifyForSub = (sub, latestPostTitle) => {
   console.log('Notifying for sub "' + sub + '"')
   notifier.notify({
-    message: 'There\'s at least one new post in /r/' + sub + '!',
+    title: '/r/' + sub + ' has a new post',
+    message: 'Click to see:\n' + latestPostTitle,
     wait: true
   }).on('click', () => {
     opn('https://www.reddit.com/r/' + sub, {
@@ -23,7 +24,7 @@ const tick = () => {
       const latestPostId = response.data.data.children[0].data.id
       const previousResponse = responses[sub] ? responses[sub] : null
       if (previousResponse !== latestPostId) {
-        notifyForSub(sub)
+        notifyForSub(sub, response.data.data.children[0].data.title)
       }
       responses[sub] = latestPostId
     }).catch(console.error)
