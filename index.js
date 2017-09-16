292e4f376a38b77b1e0e78cebc4581abfb66d4b5
Pekka <nospam@astudios.org>
2017-09-16 15:51:47 +0300

    Works.

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const axios = require('axios')
+const notifier = require('node-notifier')
+
 const subs = process.argv[2].split(',').map(sub => sub.trim())
 const responses = {}
-subs.forEach(sub => {
-  axios.get('https://www.reddit.com/r/' + sub + '/.json').then(response => {
-    const data = response.data
-    let newPostsDetected = false
-    const previousResponse = responses.sub ? responses.sub : null
-    if (!previousResponse || previousResponse !== data) {
-      newPostsDetected = true
-    }
-    responses.sub = JSON.parse(data)
+
+const notifyForSub = (sub) => {
+  console.log('Notifying for sub "' + sub + '"')
+  notifier.notify({
+    message: 'There\'s at least one new post in /r/' + sub + '!',
+    wait: true
+  }).on('click', () => {
+    console.log('CLICKED!')
   })
-})
+}
+
+const tick = () => {
+  subs.forEach(sub => {
+    axios.get('https://www.reddit.com/r/' + sub + '/.json').then(response => {
+      const latestPostId = response.data.data.children[0].data.id
+      let newPostsDetected = false
+      const previousResponse = responses[sub] ? responses[sub] : null
+      if (previousResponse !== latestPostId) {
+        newPostsDetected = true
+      }
+      responses[sub] = latestPostId
+      if (newPostsDetected) {
+        notifyForSub(sub)
+      }
+    }).catch(console.error)
+  })
+}
+
+const init = () => {
+  tick()
+  setInterval(tick, 5000)
+}
+
+init()
